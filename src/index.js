4acafb55e0cd348c38327d84677390cf39b827ec
kmthehippie <66264446+kmthehippie@users.noreply.github.com>
2023-07-21 20:26:43 +0800

    Add "this gist" and move nav functions to index
    Move the navigation buttons prev and next into the
    index.js to be imported to main.js.
    
    Also did gist.github.com/cobyism/4730490.

--- src/index.js
+++ src/index.js
@@ -25,3 +25,26 @@ navBar.forEach(nav =>{
 
 
 
+const imgW = document.querySelectorAll(".image")
+const btns2 = document.querySelectorAll(".carousel-button")
+let currentIndex = 0; 
+
+btns2.forEach(btn =>{
+    btn.addEventListener("click", ()=>{
+        imgW[currentIndex].classList.remove("show");
+        if(btn.classList[1] === "next") {
+            if (currentIndex >= imgW.length -1){
+                currentIndex = 0;
+            } else {
+                currentIndex ++
+            }            
+        } else if(btn.classList[1] === "prev") {
+            if (currentIndex <= 0){
+                currentIndex = imgW.length - 1;
+            } else {
+                currentIndex --
+            }  
+        }
+        imgW[currentIndex].classList.add("show");
+    })
+})
